5db7d571d62e06224e0fa9cbddf54317d92b1af0
Anton <pessist@rambler.ru>
2021-03-17 19:25:47 +0300

    Выполнил домашнюю роботу №7

--- HW_Algorithm7/Print.cs
+++ HW_Algorithm7/Print.cs
@@ -12,6 +12,7 @@ namespace HW_Algorithm7
         const int M = 7;
         public static void PrintSimple(int n, int m, int[,] a)
         {
+            Console.WriteLine("Количество маршрутов:");
             int i, j;
             for (i = 0; i < n; i++)
             {
@@ -20,22 +21,22 @@ namespace HW_Algorithm7
                     Console.Write(" | " + String.Format("{0,3}", a[i, j]) + " | ");
                 Console.Write("\r\n");
             }
-            Console.WriteLine($"количество путей до правой нижней клетки = {a[N - 1, M - 1]}");
+            Console.WriteLine($"\nКоличество путей до правой нижней клетки без препятствий = {a[N - 1, M - 1]}");
         }
 
         public static void PrintWithBarriers(int n, int m, int[,] a)
         {
             Console.WriteLine("\n================================================================");
-            Console.WriteLine("Количество маршрутов с припятствиями");
+            Console.WriteLine("\nКоличество маршрутов с припятствиями");
             int i, j;
-            for (i = 0; i<n; i++)
-                {
-                    Console.WriteLine("_______________________________________________________________");
-                    for (j = 0; j<m; j++)
-                        Console.Write(" | " + String.Format("{0,3}", a[i, j]) + " | ");
-                    Console.Write("\r\n");
-                }
-            Console.WriteLine($"количество путей до правой нижней клетки = {a[N - 1, M - 1]}");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("_______________________________________________________________");
+                for (j = 0; j < m; j++)
+                    Console.Write(" | " + String.Format("{0,3}", a[i, j]) + " | ");
+                Console.Write("\r\n");
             }
+            Console.WriteLine($"\nКоличество путей до правой нижней клетки с препятствиями = {a[N - 1, M - 1]}");
+        }
     }
 }
--- HW_Algorithm7/Program.cs
+++ HW_Algorithm7/Program.cs
@@ -7,38 +7,86 @@ namespace HW_Algorithm7
         const int N = 5;
         const int M = 7;
 
-        
+        public class TestCase
+        {
+            public int IndexM { get; set; }
+            public int IndexN { get; set; }
+            public int Answer { get; set; }
+        }
+
         static void Main(string[] args)
         {
+
             int[,] A = new int[N, M];
             int[,] W = new int[N, M];
-            int i, j;
 
-            for (i = 0; i < N; i++)
+            int[,] realizedA = Rectangle.SimpleNumberOfPaths(N, M, A);
+            int[,] realizedB = Rectangle.NumberOfPathsWithBarriers(N, M, W);
+
+            Console.WriteLine("\n\nТест на правильность выполнения работы по количеству маршрутов без препятствий:");
+
+            //Проверка без препятствий
+            var testData = new TestCase[2];
+
+            testData[0] = new TestCase()
             {
-                for (j = 0; j < M; j++)
+                IndexM = 1,
+                IndexN = 2,
+                Answer = 3
+            };
+            testData[1] = new TestCase()
+            {
+                IndexM = 3,
+                IndexN = 2,
+                Answer = 10
+            };
+
+            foreach (var testCase in testData)
+            {
+                var result = testCase.Answer;
+                var expected = realizedA[testCase.IndexN, testCase.IndexM];
+                if (result == expected)
                 {
-                    W[i, j] = 1;
-                    W[3, 2] = 0;
-                    W[4, 4] = 0;
-                    W[2, 3] = 0;
+                    Console.WriteLine("Все верно!");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка!");
                 }
             }
 
-            for (j = 0; j < M; j++)
-                A[0, j] = 1; // Первая строка заполнена единицами
-            for (i = 1; i < N; i++)
+            Console.WriteLine("\n\nТест на правильность выполнения работы по количеству маршрутов c препятствиями:");
+
+            //Проверка с препятствиями
+            var testData2 = new TestCase[2];
+
+            testData2[0] = new TestCase()
             {
-                A[i, 0] = 1;
-                for (j = 1; j < M; j++)
-                    A[i, j] = A[i, j - 1] + A[i - 1, j];
+                IndexM = 2,
+                IndexN = 1,
+                Answer = 3
+            };
+            testData2[1] = new TestCase()
+            {
+                IndexM = 3,
+                IndexN = 3,
+                Answer = 0
+            };
+
+
+            foreach (var testCase in testData2)
+            {
+                var result = testCase.Answer;
+                var expected = realizedB[testCase.IndexN, testCase.IndexM];
+                if (result == expected)
+                {
+                    Console.WriteLine("Все верно!");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка!");
+                }
             }
-
-
-
-            Print.PrintSimple(N, M, A);
-
-            Print.PrintWithBarriers(N, M, W);
         }
     }
 }
